3eb24763e464559ce8141b9624294c9628d83e04
murphywuwu <murphywuwu@gmail.com>
2021-01-06 00:02:57 +0800

    递归

---
+++ recur.js
@@ -0,0 +1,33 @@
+// f = f(n-1) + 1;
+
+
+// function f(n) {
+//   if (n === 1) return 1;
+
+//   return f(n-1) + 1;
+// }
+
+// // var result -> f(3) -> f(2=3-1) -> f(1=2-1)
+// //       3       2+1       1+1          1
+
+// // n! = 1 * 2 * 3 * ...*n
+// function Factotrial(n) {
+//   if(n <= 1) return 1;
+
+//   return n * fn(n-1);
+// }
+
+// function upperCase(str) {
+//   if(str.length == 1) return str.toUpperCase();
+
+//   let arr = str.split('');
+//   let c = arr.shift();
+
+//   return c.toUpperCase() + upperCase(arr.join(''));
+// }
+
+
+// console.log(upperCase('acf'));
+
+
+
